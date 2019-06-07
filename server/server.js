dbb6241b7458125e638eec671b5362e74547d821
seemasam2113 <seemasam2113@gmail.com>
2019-06-07 18:54:59 +0530

    ADDED THE app.get * FOR SERVER TO SERVE index.html FOR ANY REQUEST

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ app.use(express.static(publicPath));
 console.log('PUBLICPATH:-'+publicPath);
 
 app.get('*',(req,res)=>{
-    res.sendFile(path.join(publicPath, 'index,html'))
+    res.sendFile(path.join(publicPath, 'index.html'))
 })
 
 app.listen(port,()=>{
