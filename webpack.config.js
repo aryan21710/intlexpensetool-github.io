4ae2bc1dcbe1a8fd15c7ddb54fe148589c726d21
seemasam2113 <seemasam2113@gmail.com>
2019-06-07 18:41:17 +0530

    NO CSS WORKING APP

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,32 @@
 const path=require('path')
 console.log(path.join(__dirname, 'src', 'app.js'));
-module.exports = {
-    entry: path.join(__dirname, 'src', 'app.js'),
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename : 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        },{
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }] 
-    },
+module.exports =(env)=>{
+const isProduction= env==='production'
+    return {
+		entry: path.join(__dirname, 'src', 'app.js'),
+		output: {
+			path: path.join(__dirname, 'public'),
+			filename: 'bundle.js',
+		},
+		module: {
+			rules: [
+				{
+					loader: 'babel-loader',
+					test: /\.js$/,
+					exclude: /node_modules/,
+				},
+				{
+					test: /\.s?css$/,
+					use: ['style-loader', 'css-loader', 'sass-loader'],
+				},
+			],
+		},
 
-    devtool: 'cheap-module-eval-source-map',
-    devServer : {
-        contentBase: path.join(__dirname,'public'),
-        historyApiFallback: true
-    }
-}
+		devtool: isProduction? 'source-map' : 'cheap-module-eval-source-map',
+		devServer: {
+			contentBase: path.join(__dirname, 'public'),
+			historyApiFallback: true,
+		},
+	};
+
+} 
