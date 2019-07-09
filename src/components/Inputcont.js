a6dfd2b170950ee273845cb8ed50bdd1afe6fc78
aryan21710 <aryan21710@gmail.com>
2019-07-09 16:37:13 +0530

    ADDED THE EMAIL ID VALIDATION CHECK IN THE CLAIM FORM

--- src/components/Inputcont.js
+++ src/components/Inputcont.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './../../styles/components/inputcont.css';
-import currency from './../../public/images/currency.png'
+import currency from './../../public/images/currency.png';
 import './../../public/fonts/fontawesome-free-5.8.2-web/css/all.css';
 import FilesDemo from './InputFileComponent';
 import { SingleDatePicker } from 'react-dates';
@@ -10,136 +10,159 @@ import moment from 'moment';
 import InputNumber from 'rc-input-number';
 
 export default class Inputcont extends React.Component {
-					constructor(props) {
-						super(props);
-						this.state = {
-							text: '',
-							attachreceipt: false,
-							onclickupload: '',
-							onclicksubmitclaim: false,
-							name: '',
-							email: '',
-							createdAt: moment(),
-							calFocussed: false,
-							amount: 0
-						};
-					}
+	constructor(props) {
+		super(props);
+		this.state = {
+			text: '',
+			attachreceipt: false,
+			onclickupload: '',
+			onclicksubmitclaim: false,
+			name: '',
+			email: '',
+			createdAt: moment(),
+			calFocussed: false,
+			amount: 0,
+		};
+	}
 
-					entername = e => {
-						this.setState({
-							name: e.target.value,
-						});
-					};
+	entername = e => {
+		this.setState({
+			name: e.target.value,
+		});
+	};
 
-					enteremail = e => {
-						
-						this.setState({
-							email: e.target.value,
-						});
-					};
+	enteremail = e => {
+		console.log(e.target.value.match(/^([a-zA-Z0-9]+)(@)+([a-zA-Z0-9]+)(\.com)$/g));
+		if (e.target.value.match(/^([a-zA-Z0-9]+)(@)+([a-zA-Z0-9]+)(\.com)$/g) != 'null') {
+			this.setState({
+				email: e.target.value,
+			});
+		} else {
+			alert('ENTER A VALID EMAIL ID');
+		}
+	};
 
-					textentered = e => {
-						this.setState({
-							text: e.target.value,
-						});
-					};
+	validateemail=(e)=>{
+		if (this.state.email.match(/^([a-zA-Z0-9]+)(@)+([a-zA-Z0-9]+)(\.com)$/g) == null) {
+				alert('ENTER A VALID EMAIL ID');
+				console.log('ENTER A VALID EMAIL ID');
+			} else {
+				console.log('A VALID EMAIL ID');
+			}
+			
+	}
 
-					inputamount=(value)=>{
-						console.log(isNaN(value));
-						if (isNaN(value)) {
-							alert('PLEASE ENTER A VALID AMOUNT IN INR')
-						} else {
-							this.setState({
-								amount: value
-							})
-						}
-					}
+	textentered = e => {
+		this.setState({
+			text: e.target.value,
+		});
+	};
+
+	inputamount = value => {
+		console.log(isNaN(value));
+		if (isNaN(value)) {
+			alert('PLEASE ENTER A VALID AMOUNT IN INR');
+		} else {
+			this.setState({
+				amount: value,
+			});
+		}
+	};
+
+	submitclaim = e => {
+		e.preventDefault();
+	 
+		if (
+			this.state.text.length > 0 &&
+			this.state.name.length > 0 &&
+			this.state.email.length > 0 
+		) 
+		{
+			if (this.state.email.match(/^([a-zA-Z0-9]+)(@)+([a-zA-Z0-9]+)(\.com)$/g) == null) {
+				alert('ENTER A VALID EMAIL ID');
+				this.onclicksubmitclaim = false;
+			} else {
+				alert('CLAIM SUBMITTED SUCCESSFULLY ');
+				this.setState({
+					text: '',
+				});
+				this.setState({
+					name: '',
+				});
+				this.setState({
+					email: '',
+				});
+			} 
+	    } else {
+			alert('PLEASE FILL ALL THE FIELDS');
+			this.onclicksubmitclaim = false;
+		}
 
 
+	};
 
-					submitclaim = e => {
-						e.preventDefault();
-						if (this.state.text.length > 0 && this.state.name.length > 0 && this.state.email.length > 0) {
-							alert('CLAIM SUBMITTED SUCCESSFULLY ');
-							this.setState({
-								text: '',
-							});
-							this.setState({
-							  name: '',
-							});
-							this.setState({
-								email: '',
-							});
-						} else {
-							alert('PLEASE FILL ALL THE FIELDS');
-							this.onclicksubmitclaim = false;
-						}
-					};
+	render() {
+		return (
+			<div className="inputcont">
+				<form
+					className="submitForm"
+					// action=""
+					// method="post"
+					// enctype="multipart/form-data"
+					// onSubmit={this.submitclaim}
+				>
+					<div class="currency" />
 
-					render() {
-						return (
-							<div className="inputcont">
-								<form
-									className="submitForm"
-									// action=""
-									// method="post"
-									// enctype="multipart/form-data"
-									// onSubmit={this.submitclaim}
-								>
-									<div class="currency" />
+					<div class="forminputs">
+						<input
+							value={this.state.name}
+							placeholder="Your Name"
+							onChange={this.entername}
+							class="nameInp"
+						/>
+						<input
+							type="email"
+							value={this.state.email}
+							placeholder="Your Email"
+							onChange={this.enteremail}
+							class="nameInp"
+							onBlur={this.validateemail}
+						/>
+						<textarea
+							style={{ marginBottom: '3vh' }}
+							placeholder="Claim Description/Remarks"
+							onChange={this.textentered}
+							value={this.state.text}
+							class="nameInp"
+						/>
 
-									<div class="forminputs">
-										<input
-											value={this.state.name}
-											placeholder="Your Name"
-											onChange={this.entername}
-											class="nameInp"
-										/>
-										<input
-											type="email"
-											value={this.state.email}
-											placeholder="Your Email"
-											onChange={this.enteremail}
-											class="nameInp"
-										/>
-										<textarea
-											style={{marginBottom: '3vh'}}
-											placeholder="Claim Description/Remarks"
-											onChange={this.textentered}
-											value={this.state.text}
-											class="nameInp"
-										/>
+						<SingleDatePicker
+							date={this.state.createdAt}
+							onDateChange={createdAt => {
+								createdAt && this.setState({ createdAt });
+							}}
+							focused={this.state.calFocussed}
+							onFocusChange={({ focused }) => {
+								this.setState({ calFocussed: focused });
+							}}
+							numberOfMonths={1}
+							isOutsideRange={() => false}
+						/>
 
-										<SingleDatePicker
-											date={this.state.createdAt}
-											onDateChange={createdAt => {
-												createdAt && this.setState({ createdAt });
-											}}
-											focused={this.state.calFocussed}
-											onFocusChange={({ focused }) => {
-												this.setState({ calFocussed: focused });
-											}}
-											numberOfMonths={1}
-											isOutsideRange={() => false}
-										/>
+						<InputNumber
+							style={{ width: '50vw', marginTop: '3vh' }}
+							placeholder={'Enter Amount in INR'}
+							onChange={this.inputamount}
+							value={this.state.amount}
+						/>
 
-										<InputNumber 
-										style={{width:'50vw',marginTop: '3vh'}} 
-										placeholder={'Enter Amount in INR'}
-										onChange={this.inputamount}
-										value={this.state.amount}
-										/>
-
-										<FilesDemo />
-										<button
-											class="submitClaimBtn"
-											onClick={this.submitclaim}
-										>
-											Submit Claim
-										</button>
-									</div>
-								</form>
-							</div>
-						);
-					}
-				}
+						<FilesDemo />
+						<button class="submitClaimBtn" onClick={this.submitclaim}>
+							Submit Claim
+						</button>
+					</div>
+				</form>
+			</div>
+		);
+	}
+}
--- src/playground/validateEmailid.js
+++ src/playground/validateEmailid.js
@@ -1,19 +1,6 @@
 var validate = require("validate.js");
 
 const from="a@a.com";
-var constraints = {
-    emailId: {
-      presence: true,
-      length: {
-          minimum:7,
-          message: 'TOO SHORT TO BE AN EMAIL'
-
-      },
-      pattern:/([a-zA-Z0-9]+)(@)+([a-zA-Z0-9]+)(\.com)$/,
-      message: 'NOT A VALID EMAIL',
-
-    }
-  };
+console.log(from.match(/^([a-zA-Z0-9]+)(@)+([a-zA-Z0-9]+)(\.com)$/g))
   
 
-  validate({emailId:'a@a.com'},constraints)
